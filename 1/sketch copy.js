5791fcd55df5f4dd02ee656bdc230386abc11c87
Difei (Amber) Gao <dg3294@nyu.edu>
2023-09-27 15:07:45 -0400

    bug fixed

--- 1/sketch copy.js
+++ 1/sketch copy.js
@@ -1,14 +1,14 @@
 function rotatedRect(x, y, w, h, rotateangledegrees){
-   translate(x, y)
-   rotate(radians(rotateangledegrees))
-   rect(0,0,w,h)
-   rotate(redians(-rotateangledegrees));
-   translate(-x,-y)
+  fill(0);
+   translate(x, y);
+   rotate(radians(rotateangledegrees));
+   rect(0,0,w,h);
+   rotate(radians(-rotateangledegrees));
+   translate(-x,-y);
 
 
 
 }
-
 
 
 function setup() {
@@ -17,16 +17,18 @@ function setup() {
 
 function draw() {
   background("gold");
-
-  fill(0);
+  
   // // rect(width / 2, height/2);
 
   // rorate(radians(20));
   // rect(0,0,100,100);
 
-rotatedRect(width/2, height/2, 100,100,10)
+rotatedRect(width/2, height/2, 100,100,10);
 
-rotatedRect(width/2, height/2, 100,100,10)
+fill("pink")
+rect(100,200,60,100)
+
+// rotatedRect(100, 200, 100,100,30)
 
 
   // rect(width / 2 - width / 40, height / 4 - width / 20, width / 20, height / 2 + width / 10);
