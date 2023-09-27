6029759640ef77d447b9402cbc176f7324170fdc
Difei (Amber) Gao <dg3294@nyu.edu>
2023-09-27 15:52:23 -0400

    移动进度条gif

--- 1/sketch.js
+++ 1/sketch.js
@@ -1,11 +1,19 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background("darkred");
+  fill(0);
 }
 
+
+let xPos = 0
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2 - width / 40, height / 4 - width / 20, width / 20, height / 2 + width / 10);
-  rect(width / 2.5, height / 4 + height / 2, width - 2 * width / 2.5, width / 20);
-  rect(width / 2 - width / 20 - width / 40, height / 4 - width / 20, width / 20, width / 20);
+  ellipse(xPos,50,50,50);
+  xPos = xPos +1;
+ 
+
+
+
+
+
+
 }
